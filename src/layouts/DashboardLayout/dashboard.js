dafb62ed4e890a0b7d349022de5608084d4afb5c
Lalit Fauzdar <Fauzdar1@users.noreply.github.com>
2021-02-18 12:55:14 +0530

    Minor Changes

--- src/layouts/DashboardLayout/dashboard.js
+++ src/layouts/DashboardLayout/dashboard.js
@@ -33,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
   content: {
     flex: '1 1 auto',
     height: '100%',
-    overflow: 'auto'
+    overflow: 'scroll'
   }
 }));
 
@@ -67,6 +67,7 @@ const DashboardLayout = () => {
         <div className={classes.contentContainer}>
           <div className={classes.content}>
             <TopBar onMobileNavOpen={() => setMobileNavOpen(true)}/>
+
             <Outlet/>
             <Box
               display={'flex'}
@@ -75,8 +76,8 @@ const DashboardLayout = () => {
 
               <Fab style={{
                 position: 'absolute',
-                bottom: 33,
-                zIndex: 2,
+                bottom: 28,
+                zIndex: 1,
                 backgroundColor: '#ff5354',
                 color: selection === 2 ? '#ffffff' : '#bfbfbf',
                 outline: 'none'
@@ -86,20 +87,20 @@ const DashboardLayout = () => {
               }}>
                 <Home/>
               </Fab>
+
               <BottomNavigation
                 showLabels
                 className={getRoleColor()}
                 style={{
-                  borderRadius: '10px',
-                  borderTopLeftRadius: '10px',
-                  borderTopRightRadius: '10px',
-                  position: 'absolute',
-                  bottom: 5,
-                  zIndex: 1,
+                  borderTopLeftRadius: '30px',
+                  borderTopRightRadius: '30px',
+                  position: 'fixed',
+                  bottom: 0,
+                  flex: '1 1 auto',
                   flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  paddingHorizontal: 15,
-                  paddingVertical: 10,
+                  justifyContent: 'center',
+                  paddingHorizontal: 0,
+                  paddingVertical: 0,
                   color: '#fff'
                 }}
               >
