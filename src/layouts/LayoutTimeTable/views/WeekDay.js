e6b50c4fdaa81418b1685cf3f3c17dde0fd3b9c3
Mohak <mohakgupta2000@gmail.com>
2021-03-03 17:25:35 +0530

    timetable edit working done

--- src/layouts/LayoutTimeTable/views/WeekDay.js
+++ src/layouts/LayoutTimeTable/views/WeekDay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   Box,
-  Button,
+  Button, Divider,
   FormControl, Grid, InputLabel,
   MenuItem, Paper, Select,
   TextField
@@ -16,7 +16,7 @@ class WeekDay extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {data: [[{type: "text", index: "00", value: "Courses"}]]};
+    this.state = {data: [[{type: "text", index: "00", value: "Courses/Timings"}]]};
   }
 
   returnState() {
@@ -27,7 +27,6 @@ class WeekDay extends React.Component {
     this.props.callback(obj)
   }
 
-
   addRow() {
     let data = this.state.data;
     let max = 0;
@@ -36,12 +35,12 @@ class WeekDay extends React.Component {
         max = data[i].length;
       }
     }
-    data.push([{value: '', index: `${iMatrix}0`, type: "course"}])
+    data.push([{value: '', index: `${iMatrix}0`, type: "course"}]);
     for (let i = 1; i < max; i++) {
-      data[data.length - 1].push({value: '', index: `${iMatrix}${i}`, type: "teacherSubject"})
+      data[data.length - 1].push({value: {subject: "", teacher: ""}, index: `${iMatrix}${i}`, type: "teacherSubject"})
     }
     iMatrix++;
-    this.setState(data)
+    this.setState(data);
   }
 
   removeRow() {
@@ -55,12 +54,12 @@ class WeekDay extends React.Component {
 
   addColumn() {
     let data = this.state.data;
-    data[0].push({value: '', type: 'text', index: `0${jMatrix}`})
+    data[0].push({value: '', type: 'text', index: `0${jMatrix}`});
     for (let i = 1; i < data.length; i++) {
       data[i].push({value: {subject: "", teacher: ""}, type: "teacherSubject", index: `${i}${jMatrix}`})
     }
     jMatrix++;
-    this.setState(data)
+    this.setState(data);
   }
 
   removeColumn() {
@@ -68,7 +67,7 @@ class WeekDay extends React.Component {
     let data = this.state.data;
 
     for (let i = 0; i < data.length; i++) {
-      if (data[i].length != 1) {
+      if (data[i].length !== 1) {
         data[i].pop();
       }
     }
@@ -105,7 +104,6 @@ class WeekDay extends React.Component {
       }
     }
     this.setState(sData);
-
   }
 
   handleSubjectChange(e, index) {
@@ -124,7 +122,6 @@ class WeekDay extends React.Component {
     console.log(this.state.data)
   }
 
-
   render() {
     const {classes} = this.props;
 
@@ -136,7 +133,6 @@ class WeekDay extends React.Component {
             flexDirection="row"
           ><Box
             width={'100px'}
-
             style={{
               display: "flex",
               backgroundColor: 'white',
@@ -154,122 +150,164 @@ class WeekDay extends React.Component {
             >
               {this.state.data.map(data => {
                 return (
-                  <Grid
-                    container
-                    item xs={12}
-                    spacing={2}
+                  <>
+                    <Box p={1.01}/>
+                    <Grid
+                      container
+                      item xs={12}
+                      spacing={2}
 
-                  >
-                    <React.Fragment>
-                      {
-                        data.map(res => {
-                          if (res.type === 'text') {
-                            return (
-                              <Grid item xs={2}>
-                                <TextField
-                                  value={res.value}
-                                  onChange={e => this.handleTextChange(e, res.index)}
-                                  placeholder="Period Timings"
-                                  variant="outlined"
-                                />
-                              </Grid>
-                            )
-                          }
-                          if (res.type === 'course') {
-                            return (
-                              <Grid item xs={2}>
-                                <FormControl
-                                  style={{
-                                    minWidth: 120,
-                                    width: '100%',
-
-                                  }}
-                                >
-                                  <InputLabel
-                                    id={res.index}
-                                    style={{marginLeft: '10px'}}
-                                  >Course</InputLabel>
-                                  <Select
-                                    labelId={res.index}
-                                    id={res.index}
-                                    value={res.value}
-                                    onChange={e => this.handleCourseChange(e, res.index)}
-                                    label="Course"
-                                    variant="outlined"
+                    >
+                      <React.Fragment>
+                        {
+                          data.map(res => {
+                            if (res.type === 'text') {
+                              return (
+                                <>
+                                  <Grid item xs={2}
+                                        style={{
+                                          border: "1px solid gray"
+                                        }}
                                   >
-                                    <MenuItem value={'B.Tech'}>B.Tech</MenuItem>
-                                    <MenuItem value={'MBA'}>MBA</MenuItem>
-                                    <MenuItem value={'BSc'}>B.Sc</MenuItem>
-                                  </Select>
-                                </FormControl>
-                              </Grid>
-                            )
-                          }
-                          if (res.type === 'teacherSubject') {
-                            return (
-                              <Grid item xs={2}>
-                                <FormControl
-                                  style={{
-                                    minWidth: 120,
-                                    width: '100%',
+                                    <TextField
+                                      value={res.value}
+                                      onChange={e => this.handleTextChange(e, res.index)}
+                                      placeholder="Period Timings"
+                                      variant="filled"
+                                      style={{color: "blue"}}
+                                    />
 
+                                  </Grid>
+                                  <Box mt={2}/>
+                                </>
+                              )
+                            }
+                            if (res.type === 'course') {
+                              return (
+                                <>
 
-                                  }}
-                                >
-                                  <InputLabel
-                                    id={res.index}
-                                    style={{marginLeft: '10px'}}
-                                  >Subject</InputLabel>
-                                  <Select
-                                    labelId={res.index}
-                                    id={res.index}
-                                    value={res.value.subject}
-                                    onChange={e => this.handleSubjectChange(e, res.index)}
-                                    variant="outlined"
+                                  <Grid item xs={2}
+                                        style={{
+                                          border: "1px solid gray",
+                                          justifyContent: 'center',
+                                          alignItems: 'center',
+                                        }}
+
                                   >
-                                    <MenuItem value={'Digital Electronics'}>Digital Electronics</MenuItem>
-                                    <MenuItem value={'AEC'}>AEC</MenuItem>
-                                    <MenuItem value={'DSA'}>DSA</MenuItem>
-                                  </Select>
-                                </FormControl>
-                                <br/>
-                                <FormControl
-                                  style={{
-                                    minWidth: 120,
-                                    width: '100%'
-                                  }}
+                                    <FormControl
+                                      style={{
+                                        minWidth: 120,
+                                        width: '100%',
+                                      }}
+                                    >
+                                      <InputLabel
+                                        id={res.index}
+                                        style={{
+                                          marginLeft: '10px'
+                                        }}
+                                      >Course</InputLabel>
+                                      <Select
+                                        labelId={res.index}
+                                        id={res.index}
+                                        value={res.value}
+                                        onChange={e => this.handleCourseChange(e, res.index)}
+                                        label="Course"
+                                        variant="filled"
+                                      >
+                                        <MenuItem value={'B.Tech'}>B.Tech</MenuItem>
+                                        <MenuItem value={'MBA'}>MBA</MenuItem>
+                                        <MenuItem value={'BSc'}>B.Sc</MenuItem>
+                                      </Select>
+                                    </FormControl>
+                                  </Grid>
+                                </>
+                              )
+                            }
+                            if (res.type === 'teacherSubject') {
+                              return (
+                                <Grid item xs={2}
+                                      style={{
+                                        border: "1px solid gray"
+                                      }}
                                 >
-                                  <InputLabel
-                                    id={res.index}
-                                    style={{marginLeft: '10px'}}
-                                  >Teacher</InputLabel>
-                                  <Select
-                                    labelId={res.index}
-                                    id={res.index}
-                                    value={res.value.teacher}
-                                    onChange={e => this.handleTeacherChange(e, res.index)}
-                                    variant="outlined"
+                                  <Grid
+
                                   >
-                                    <MenuItem value={'Some teacher'}>Some teacher</MenuItem>
-                                    <MenuItem value={'Buddha'}>Buddha</MenuItem>
-                                    <MenuItem value={'Someone'}>Someone</MenuItem>
-                                  </Select>
-                                </FormControl>
-                              </Grid>
-                            )
-                          }
+                                    <FormControl
+                                      style={{
+                                        minWidth: 120,
+                                        width: '100%',
 
-                        })
-                      }
-                    </React.Fragment>
+                                      }}
+                                    >
+                                      <InputLabel
+                                        id={res.index}
+                                        style={{
+                                          marginLeft: '10px',
+                                          color: "red"
+                                        }}
+                                      >Subject</InputLabel>
+                                      <Select
+                                        labelId={res.index}
+                                        id={res.index}
+                                        value={res.value.subject}
+                                        onChange={e => this.handleSubjectChange(e, res.index)}
+                                        variant="filled"
+                                      >
+                                        <MenuItem value={'Digital Electronics'}>Digital Electronics</MenuItem>
+                                        <MenuItem value={'AEC'}>AEC</MenuItem>
+                                        <MenuItem value={'DSA'}>DSA</MenuItem>
+                                      </Select>
+                                    </FormControl>
+                                  </Grid>
 
-                  </Grid>
+                                  <Box mt={1}/>
+                                  <Grid
+
+                                  >
+                                    <FormControl
+                                      style={{
+                                        minWidth: 120,
+                                        width: '100%'
+                                      }}
+                                    >
+                                      <InputLabel
+                                        id={res.index}
+                                        style={{
+                                          marginLeft: '10px',
+                                          color: "green"
+                                        }}
+                                      >Teacher</InputLabel>
+                                      <Select
+                                        labelId={res.index}
+                                        id={res.index}
+                                        value={res.value.teacher}
+                                        onChange={e => this.handleTeacherChange(e, res.index)}
+                                        variant="filled"
+                                      >
+                                        <MenuItem value={'Some teacher'}>Some teacher</MenuItem>
+                                        <MenuItem value={'Buddha'}>Buddha</MenuItem>
+                                        <MenuItem value={'Someone'}>Someone</MenuItem>
+                                      </Select>
+                                    </FormControl>
+                                  </Grid>
+
+                                  <Box mt={1}/>
+
+                                  <Box mt={1}/>
+                                </Grid>
+                              )
+                            }
+
+                          })
+                        }
+                      </React.Fragment>
+                    </Grid>
+                  </>
                 )
               })
               }
-
             </Grid>
-
           </Box>
           <Button onClick={this.addRow.bind(this)}><AddIcon/> Row</Button>
           <Button onClick={this.removeRow.bind(this)}><RemoveIcon/> Row</Button>
